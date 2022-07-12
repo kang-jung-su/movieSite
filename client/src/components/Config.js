28323a247656818e3d1820818099d9405b25e9c9
jskang <wjdtnrkd0203@naver.com>
2022-07-12 21:18:16 +0900

    movie grid & detail page complete

---
+++ client/src/components/Config.js
@@ -0,0 +1,11 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users'; 
+
+//api key
+export const API_KEY = 'edc6c92f43200b2eb3121e02acbf1c94';
+
+//api URL
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+//이미지 가져오는 경로
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
